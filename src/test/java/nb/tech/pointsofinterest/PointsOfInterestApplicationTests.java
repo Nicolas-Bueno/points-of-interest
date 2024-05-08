613d2ff980cdab5a6b680cd1050737e236e7696d
Nicolas Bueno <nicolasbrian202@gmail.com>
2024-05-08 13:47:18 -0300

    first commit

---
+++ src/test/java/nb/tech/pointsofinterest/PointsOfInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package nb.tech.pointsofinterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsOfInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
