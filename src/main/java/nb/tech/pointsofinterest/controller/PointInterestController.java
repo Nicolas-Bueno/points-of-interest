e160aeb715678e413082ec74bdc67c0ed1eff806
Nicolas Bueno <nicolasbrian202@gmail.com>
2024-05-08 19:30:05 -0300

    listar pontos de interesse

--- src/main/java/nb/tech/pointsofinterest/controller/PointInterestController.java
+++ src/main/java/nb/tech/pointsofinterest/controller/PointInterestController.java
@@ -6,9 +6,13 @@ import nb.tech.pointsofinterest.entity.PointInterest;
 import nb.tech.pointsofinterest.repository.PointInterestRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -24,6 +28,13 @@ public class PointInterestController {
         return ResponseEntity.ok().build();
     }
 
-   
+    @GetMapping("/points-of-interest")
+    public ResponseEntity<Page<PointInterest>> ListPoint(@RequestParam(name = "page", defaultValue = "0")Integer page,
+                                        @RequestParam(name = "pageSize", defaultValue = "10")Integer pageSize) {
+
+        var body = pointInterestRepository.findAll(PageRequest.of(page, pageSize));
+        
+        return ResponseEntity.ok(body);
+    }
     
 }
